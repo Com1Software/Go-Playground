0798acdd410ebd48f71844de98e92723791e1752
Com1 Software, Inc <information@com1software.com>
2022-09-18 14:41:41 -0400

    Update playground.go

--- playground/playground.go
+++ playground/playground.go
@@ -8,6 +8,25 @@ import (
 //
 //------------------ (c) 1992-2022 Com1 Software Development
 //
+
+
+// Openbrowser : Opens default web browser to specified url
+func Openbrowser(url string) error {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
 // Start Page :
 func StartPage() string {
 	xdata := "<!DOCTYPE html>"
